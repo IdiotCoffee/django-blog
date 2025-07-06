795c686b3be099f1b108fa69358daf84a5040952
IdiotCoffee <ishaanmavinkurve16.com>
2025-07-06 10:50:01 +0530

    templates added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def  homePage(request):
+    return render(request, "blog/index.html")
+
+def posts(request):
+    pass
+
+def singleBlog(request):
+    pass
