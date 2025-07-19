8235b8b17f0cfc29d2306a1c2bf43481ff453e03
ishaan <ishaanmavinkurve16.com>
2025-07-19 16:21:58 +0530

    settings changed, requirements.txt added

--- django_website/settings.py
+++ django_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-36ohe3-u6pr^(wd42w1u3qq1-6nidewd5#stx8ha7^h(98cz5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
